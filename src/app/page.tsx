2bc0918c07d843daed33a6e20cdea194eef9ea56
Abdullah Ahmed <contactabdullahahmed@gmail.com>
2025-08-05 13:32:48 +0500

    feat: add tailwindcss-animate package and implement animations in components; create layout and page structure for Customers, Home, and Tickets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Home() {
     return (
-        <div className='bg-cover bg-center' style={{ backgroundImage: "url('/images/home-img.png')" }}>
+        <div className='bg-cover bg-home-img bg-center'>
             <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
                 <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
                     <h1 className="text-4xl font-bold">
